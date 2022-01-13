a98b36b4cbe3f8d5734db9a2f1c4eca528163396
singhalshruti31 <97431303+singhalshruti31@users.noreply.github.com>
2022-01-13 19:45:20 +0530

    Add files via upload
    changes done

--- Python/Activities/Activity10.py
+++ Python/Activities/Activity10.py
@@ -1,6 +1,6 @@
 t = tuple(input("Enter the numbers separated by single space :: ").split(" "))
-print(t)
-
+print("Entered tuple is", str(t))
+print("Number divisible by 5 :: ")
 for i in t:
     if int(i)%5 == 0:
         print(i)
